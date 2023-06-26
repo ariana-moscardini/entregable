d954ae188b87f983d5bc142b916e8a6f39c03d6d
ariana moscardini <moscardiniariana@gmail.com>
2023-06-26 19:28:07 -0300

    agrego de productos y server

--- app.js
+++ app.js
@@ -2,37 +2,43 @@ const express = require('express');
 const ProductManager = require('./ProductManager');
 
 const app = express();
-const productManager = new ProductManager('productos.json');
+const productManager = new ProductManager('./productos.json');
 
+app.get('/products', async (req, res) => {
+    try {
+        const { limit } = req.query;
 
-app.get('/products', (req, res) => {
-    const { limit } = req.query;
+        let products = await productManager.getProducts();
 
-    let products = productManager.getProducts();
-
-    if (limit) {
+        if (limit) {
         const parsedLimit = parseInt(limit, 10);
         products = products.slice(0, parsedLimit);
     }
 
     res.json({ products });
+        } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los productos' });
+        }
 });
 
+app.get('/products/:pid', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.pid, 10);
 
-app.get('/products/:pid', (req, res) => {
-    const productId = parseInt(req.params.pid, 10);
+        const product = await productManager.getProductById(productId);
 
-    const product = productManager.getProductById(productId);
-
-    if (product) {
+        if (product) {
         res.json({ product });
-    } else {
+        } else {
         res.status(404).json({ error: 'Producto no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener el producto' });
     }
 });
-
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Servidor Express escuchando en el puerto ${port}`);
 });
+
